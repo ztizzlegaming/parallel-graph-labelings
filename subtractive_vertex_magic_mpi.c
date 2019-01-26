4acaa53176d7603a9e82c0bec53f850b6aa648cf
Jordan Turley <ztizzlegaming@gmail.com>
2019-01-25 21:03:09 -0500

    Update MPI version to use gatherv

--- subtractive_vertex_magic_mpi.c
+++ subtractive_vertex_magic_mpi.c
@@ -21,6 +21,7 @@
 #include <mpi.h>
 
 #include <algorithm>
+#include <vector>
 
 #define OUTPUT_FILE_LINE_SIZE 1000
 #define DEVELOPMENT 1 //Print out the permutations and write to file
@@ -29,6 +30,7 @@
 struct Graph readGraph(char* filename);
 struct Graph generateGraph(int cycleSize, int connectingVertices);
 long factorial(int n);
+int* generatePermutation(long idx, int permSize);
 void printArr(int arr[], int size, int newline);
 
 //Struct for a graph, holding the number of vertices, edges, and the
@@ -55,7 +57,6 @@ int main(int argc, char *argv[]) {
 
 	int returnVal;
 	returnVal = MPI_Init(&argc, &argv);
-	printf("Return val: %d\n", returnVal);
 	if (returnVal != MPI_SUCCESS) {
 		printf("An error occured during MPI_Init. Terminating.\n");
 		MPI_Abort(MPI_COMM_WORLD, returnVal);
@@ -129,73 +130,20 @@ int main(int argc, char *argv[]) {
 		MPI_Bcast(matrix[i1], vertices, MPI_INT, 0, MPI_COMM_WORLD);
 	}
 	
-	int* permutation = (int *) malloc(permSize * sizeof(int));
-	for (int i1 = 0; i1 < permSize; i1++) {
-		permutation[i1] = i1 + 1;
-	}
-	if (start != 0) {
-		//Generate perm for start using factoradic numbers
-		int factoradicLength = 0;
-		long factorials[permSize];
-		long fac = 1;
-		int count = 0;
-		while (start >= fac) {
-			fac = factorial(factoradicLength);
-			factorials[count] = fac;
-			count++;
-			factoradicLength++;
-		}
-		factoradicLength--;
+	int* permutation = generatePermutation(start, permSize);
 
-		long factoradic[permSize];
-		for (int i1 = 0; i1 < permSize; i1++) {
-			factoradic[i1] = 0;
-		}
+	time_t startTime;
+	time_t finishTime;
 
-		long cur = start;
-		count = 0;
-		for (int i1 = factoradicLength - 1; i1 >= 0; i1--) {
-			long fNum = cur / factorials[i1];
-			factoradic[count + permSize - factoradicLength] = fNum;
-			count++;
-			cur = cur % factorials[i1];
-		}
+	startTime = time(NULL);
 
-		int permutationTemp[permSize];
-		permutationTemp[0] = factoradic[0] + 1;
-		permutation[factoradic[0]] = -1;
-		for (int i1 = 1; i1 < permSize; i1++) {
-			int fNum = factoradic[i1];
-			int permIdx = 0;
-			int actualIdx = 0;
-			while (permIdx != fNum || permutation[actualIdx] == -1) {
-				if (permutation[actualIdx] != -1) {
-					permIdx++;
-				}
-				actualIdx++;
-			}
-			permutationTemp[i1] = permutation[actualIdx];
-			permutation[actualIdx] = -1;
-		}
-
-		for (int i1 = 0; i1 < permSize; i1++) {
-			permutation[i1] = permutationTemp[i1];
-		}
-	}
-
-	//Output file to store permutations
-	//Write the graph parameters to the first line
-	char filename[OUTPUT_FILE_LINE_SIZE];
-	sprintf(filename, "output_proc=%d_%d_%d.txt", rank, cycleSize, connectingVertices);
-	FILE* outputFile = fopen(filename, "w");
-	char firstLine[OUTPUT_FILE_LINE_SIZE];
-	sprintf(firstLine, "Graph: Cycle size = %d, connecting vertices = %d\n",
-		cycleSize, connectingVertices);
-	fputs(firstLine, outputFile);
+	//Init vector to hold permutation indexes that work
+	std::vector<long> worksIdxs;
+	std::vector<int> magicNumbers;
 
 	//Loop over the permutations and check the graphs
 	int magicNumber, firstVertex, works, curVertexValue, vertex, edge, vertexIn, vertexOut;
-	for (long i1 = start; i1 < end; i1++) {
+	for (long permIdx = start; permIdx < end; permIdx++) {
 		magicNumber = 0;
 		firstVertex = 1;
 		works = 1;
@@ -235,19 +183,104 @@ int main(int argc, char *argv[]) {
 		}
 
 		if (works) {
-			if (MODE == DEVELOPMENT) {
-				printArr(permutation, permSize, 0);
-				printf(" Magic Number = %d\n", magicNumber);
+			worksIdxs.push_back(permIdx);
+			magicNumbers.push_back(magicNumber);
+		}
+
+		std::next_permutation(permutation, permutation + permSize);
+	}
+
+	//Wait for all to finish before the end time is calculated
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	finishTime = time(NULL);
+	double timeTaken = difftime(finishTime, startTime);
+
+	//Do a gather to get the sizes of each of the arrays
+	int size = worksIdxs.size();
+	int* sizes = NULL;
+	if (rank == 0) {
+		sizes = (int *) malloc(numThreads * sizeof(int));
+	}
+
+	//Gather the sizes to the root
+	MPI_Gather(&size, 1, MPI_INT, sizes, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	//Init arrays for works idxs, magic numbers, and displacements
+	long* allWorksIdxs = NULL;
+	int* allMagicNumbers = NULL;
+	int* displs = NULL;
+
+	//Calculate total number of perms that work and displacement array
+	int numPermsWork;
+	if (rank == 0) {
+		numPermsWork = 0;
+		for (int i1 = 0; i1 < numThreads; i1++) {
+			numPermsWork += sizes[i1];
+		}
+		allWorksIdxs = (long *) malloc(numPermsWork * sizeof(long));
+		allMagicNumbers = (int *) malloc(numPermsWork * sizeof(int));
+		displs = (int *) malloc(numThreads * sizeof(int));
+
+		int displ = 0;
+		for (int i1 = 0; i1 < numThreads; i1++) {
+			displs[i1] = displ;
+			displ += sizes[i1];
+		}
+	}
+
+	//Use gatherv to get the indices and magic numbers from each process
+	MPI_Gatherv(&worksIdxs[0], size, MPI_LONG, allWorksIdxs, sizes, displs, MPI_LONG, 0, MPI_COMM_WORLD);
+	MPI_Gatherv(&magicNumbers[0], size, MPI_INT, allMagicNumbers, sizes, displs, MPI_INT, 0, MPI_COMM_WORLD);
+
+	if (rank == 0) {
+		//Output file to store permutations
+		//Write the graph parameters to the first line
+		char filename[OUTPUT_FILE_LINE_SIZE];
+		sprintf(filename, "output_%d_%d.txt", cycleSize, connectingVertices);
+		FILE* outputFile = fopen(filename, "w");
+		char firstLine[OUTPUT_FILE_LINE_SIZE];
+		sprintf(firstLine, "Graph: Cycle size = %d, connecting vertices = %d\n",
+			cycleSize, connectingVertices);
+		fputs(firstLine, outputFile);
+
+		//Write the time taken to the file
+		char timeLine[OUTPUT_FILE_LINE_SIZE];
+		sprintf(timeLine, "Time taken: %f seconds\n", timeTaken);
+		fputs(timeLine, outputFile);
+
+		//Write the adjacency matrix to the output file
+		char matrixLine[OUTPUT_FILE_LINE_SIZE] = "";
+		for (int i1 = 0; i1 < vertices; i1++) {
+			for (int i2 = 0; i2 < vertices; i2++) {
+				int edge = matrix[i1][i2];
+				char part[3];
+				sprintf(part, "%d ", edge);
+				strcat(matrixLine, part);
 			}
+			//Add \n to the end
+			char end[2] = "\n";
+			strcat(matrixLine, end);
+		}
+		//Write line to file
+		fputs(matrixLine, outputFile);
+
+		for (int i1 = 0; i1 < numPermsWork; i1++) {
+			long permIdx = allWorksIdxs[i1];
+			int magicNumber = allMagicNumbers[i1];
+			int* perm = generatePermutation(permIdx, permSize);
+			printArr(perm, permSize, 0);
+			printf(" Magic number: %d\n", magicNumber);
 
 			//Write the permutation to the output file
-			char outputLine[OUTPUT_FILE_LINE_SIZE] = "{";
+			char outputLine[OUTPUT_FILE_LINE_SIZE] = "";
+			sprintf(outputLine, "%d: {", (i1 + 1));
 			for (int i1 = 0; i1 < permSize; i1++) {
 				char part[10]; //Shouldn't ever be bigger than 6, 10 to be save
 				if (i1 != permSize - 1) {
-					sprintf(part, "%d, ", permutation[i1]);
+					sprintf(part, "%d, ", perm[i1]);
 				} else {
-					sprintf(part, "%d", permutation[i1]);
+					sprintf(part, "%d", perm[i1]);
 				}
 				strcat(outputLine, part);
 			}
@@ -255,15 +288,26 @@ int main(int argc, char *argv[]) {
 			sprintf(end, "} Magic Number: %d\n", magicNumber);
 			strcat(outputLine, end);
 			fputs(outputLine, outputFile);
+
+			free(perm);
 		}
 
-		std::next_permutation(permutation, permutation + permSize);
+		//Close the file
+		if (outputFile != NULL) {
+			fclose(outputFile);
+		}
 	}
 
+	/* if (rank == 0) {
+		for (int i1 = 0; i1 < numThreads; i1++) {
+			printf("%d\n", sizes[i1]);
+		}
+	} */
+
 	//Close the file
-	if (outputFile != NULL) {
-		fclose(outputFile);
-	}
+	//if (outputFile != NULL) {
+	//	fclose(outputFile);
+	//}
 
 	//Free the dynamic array for the graph
 	for (int i1 = 0; i1 < vertices; i1++) {
@@ -395,6 +439,68 @@ long factorial(int n) {
 	}
 
 	return n * factorial(n - 1);
+}
+
+int* generatePermutation(long idx, int permSize) {
+	int* permutation = (int *) malloc(permSize * sizeof(int));
+	for (int i1 = 0; i1 < permSize; i1++) {
+		permutation[i1] = i1 + 1;
+	}
+	if (idx != 0) {
+		//Generate perm for idx using factoradic numbers
+		int factoradicLength = 0;
+		long factorials[permSize];
+		long fac = 1;
+		int count = 0;
+		while (idx >= fac) {
+			fac = factorial(factoradicLength);
+			factorials[count] = fac;
+			count++;
+			factoradicLength++;
+		}
+		factoradicLength--;
+		//printf("Factoradic length: %d\n", factoradicLength);
+
+		long factoradic[permSize];
+		for (int i1 = 0; i1 < permSize; i1++) {
+			factoradic[i1] = 0;
+		}
+
+		//printf("Array inited\n");
+
+		long cur = idx;
+		count = 0;
+		for (int i1 = factoradicLength - 1; i1 >= 0; i1--) {
+			long fNum = cur / factorials[i1];
+			//printf("fNum = %ld\n", fNum);
+			factoradic[count + permSize - factoradicLength] = fNum;
+			count++;
+			cur = cur % factorials[i1];
+		}
+
+		int permutationTemp[permSize];
+		permutationTemp[0] = factoradic[0] + 1;
+		permutation[factoradic[0]] = -1;
+		for (int i1 = 1; i1 < permSize; i1++) {
+			int fNum = factoradic[i1];
+			int permIdx = 0;
+			int actualIdx = 0;
+			while (permIdx != fNum || permutation[actualIdx] == -1) {
+				if (permutation[actualIdx] != -1) {
+					permIdx++;
+				}
+				actualIdx++;
+			}
+			permutationTemp[i1] = permutation[actualIdx];
+			permutation[actualIdx] = -1;
+		}
+
+		for (int i1 = 0; i1 < permSize; i1++) {
+			permutation[i1] = permutationTemp[i1];
+		}
+	}
+
+	return permutation;
 }
 
 void printArr(int arr[], int size, int newline) {
